b72249945b62a331a9fd616940eb68ea9be93ebf
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2016-11-24 12:12:59 +0100

    fixed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "contails#show"
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create, :destroy]
   end
